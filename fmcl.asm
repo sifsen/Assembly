3469f16f247a53aeca75a64a507baacc29406f25
Sen <57877797+sifsen@users.noreply.github.com>
2025-03-26 15:50:47 +0100

    Update fmcl.asm

--- fmcl.asm
+++ fmcl.asm
@@ -1,16 +1,18 @@
 section .data
-        msg db 'Fuck my chungus life', 0x0A
+	msg db 'Fuck my chungus life', 0x0A
+	; msg is a string in memory, and db means "define bytes".
+	; "0x0A" is the same as EscapeN in C.
 
 section .text
-        global _start
-
+	global _start
+	; global _start will tell the assembler where to begin.
 _start:
-        mov eax, 4
-        mov ebx, 1
-        mov ecx, msg
-        mov edx, 20
-        int 0x80
+	mov eax, 4   	; syscall nr. 4 -> sys_write
+	mov ebx, 1   	; file descriptor 1 -> stdout
+	mov ecx, msg 	; pointer to message
+	mov edx, 20  	; message length (20 bytes)
+	int 0x80     	; make syscall
 
-        mov eax, 1
-        xor ebx, ebx
-        int 0x80
+	mov eax, 1	; syscall nr. 1 -> sys_exit
+	xor ebx, ebx	; exit code 0 (same as return 0)
+	int 0x80	; make syscall
